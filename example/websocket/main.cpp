d47cc5e350fcb88c4cbe1b0835882d671fdacdca
Akira <akira.matsuda@me.com>
2019-11-20 17:29:01 +0900

    Allow tls.

--- example/websocket/main.cpp
+++ example/websocket/main.cpp
@@ -14,17 +14,22 @@ int main(int argc, char *argv[])
 	std::once_flag once;
 	std::call_once(once, savanna::load_root_cert, m2d::root_cert, *savanna::shared_ssl_ctx());
 
-	savanna::url url("ws://localhost:5001");
-	savanna::websocket::session session(url);
-	session.on_message([](beast::flat_buffer buffer) {
-		std::cout << beast::make_printable(buffer.data()) << std::endl;
-	});
-	session.connect();
+	try {
+		savanna::url url("wss://echo.websocket.org:443");
+		savanna::websocket::session session(url);
+		session.on_message([](beast::flat_buffer buffer) {
+			std::cout << beast::make_printable(buffer.data()) << std::endl;
+		});
+		session.connect();
+		std::cout << "connected" << std::endl;
 
-	while (true) {
-		std::cout << "send" << std::endl;
-		session.send("hello");
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		while (true) {
+			std::cout << "send" << std::endl;
+			session.send("hello");
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+	} catch (std::exception e) {
+		std::cout << "try error: " << e.what() << std::endl;
 	}
 
 	return 0;
